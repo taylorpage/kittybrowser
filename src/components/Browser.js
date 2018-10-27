094dc48b5d5411bd6a564663ff30bdae88c2aabb
Taylor Page <jonathantaylorpage@gmail.com>
2018-10-27 12:53:43 -0700

    Add KittyCore ABI and connect contract
    - Create KittyCoreABI.json and add the ABI from the contract address
    - Uncomment code that creates contract and adds the contract to drizzle
    
    Signed off by: Taylor Page <jontaylorpage@gmail.com>

--- src/components/Browser.js
+++ src/components/Browser.js
@@ -1,27 +1,27 @@
 import React, { Component } from 'react';
 import { object } from 'prop-types';
-// import Web3 from 'web3';
-// import KittyCoreABI from 'contracts/KittyCoreABI.json';
-// import { CONTRACT_NAME, CONTRACT_ADDRESS } from '../config';
+import Web3 from 'web3';
+import KittyCoreABI from '../contracts/KittyCoreABI.json';
+import { CONTRACT_NAME, CONTRACT_ADDRESS } from '../config';
 
 class Browser extends Component {
-  
+
   componentDidMount() {
-    // const web3 = new Web3(window.web3.currentProvider);
+    const web3 = new Web3(window.web3.currentProvider);
 
     // Initialize the contract instance
 
-    // const kittyContract = new web3.eth.Contract(
-    //   KittyCoreABI, // import the contracts's ABI and use it here
-    //   CONTRACT_ADDRESS,
-    // );
+    const kittyContract = new web3.eth.Contract(
+      KittyCoreABI, // import the contracts's ABI and use it here
+      CONTRACT_ADDRESS,
+    );
 
     // Add the contract to the drizzle store
 
-    // this.context.drizzle.addContract({
-    //   contractName: CONTRACT_NAME,
-    //   web3Contract: kittyContract,
-    // });
+    this.context.drizzle.addContract({
+      contractName: CONTRACT_NAME,
+      web3Contract: kittyContract,
+    });
   }
 
   render() {
